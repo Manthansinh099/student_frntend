f8dae122ab162bd9292b85e0ed84e5a3a6b94870
Manthansinh099 <88449523+Manthansinh099@users.noreply.github.com>
2022-09-12 23:34:42 +0530

    Added Edit/Delete Buttons to Students Listing

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Card, Container, Table } from "react-bootstrap";
+import { Card, Container, Table, ButtonGroup, Button } from "react-bootstrap";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function StudentList() {
   const [students, setStudents] = useState([]);
-
   useEffect(() => {
     getStudents();
   }, []);
-
   let getStudents = () => {
     axios
       .get("http://localhost:8080/listStudents")
@@ -24,10 +24,11 @@ export default function StudentList() {
         <Card.Body>
           <Table striped bordered hover>
             <thead>
-            <tr>
+              <tr>
                 <th>Student Id</th>
                 <th>Student Name</th>
                 <th>Student Address</th>
+                <th>Edit/Delete</th>
               </tr>
             </thead>
             <tbody>
@@ -41,6 +42,12 @@ export default function StudentList() {
                   <td>{student.id}</td>
                   <td>{student.name}</td>
                   <td>{student.address}</td>
+                  <td>
+                    <ButtonGroup>
+                      <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button>{ ' '}
+                      <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                    </ButtonGroup>
+                  </td>
                 </tr>
                 )
               )}
