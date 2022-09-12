fa176d9727571c95306de2777c0869bbbaa2aaf6
Manthansinh099 <88449523+Manthansinh099@users.noreply.github.com>
2022-09-13 00:22:16 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
 import NavigationBar from './components/NavigationBar';
 import Footer from './components/Footer';
 import Container from 'react-bootstrap/esm/Container'; 
-
+import { Router } from 'react-router-dom';
+import {  Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
+    
     <>
-    <div className="App">
+      <div className="App">
+      <Router>
       <NavigationBar/>
-      <Container/>
-      <Student/>
-      <StudentList/>
-      <Container/>
+      <Container>
+        <Routes>
+      <Route path="student" element={<Student/>}/>
+      <Route path="listStudents" element={<StudentList/>}/>
+      </Routes>
+      </Container>
       <Footer/>
+      </Router>
     </div>
     </>
+    
   );
 
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,14 +2,24 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-
+import { Link } from 'react-router-dom';
 export default function NavigationBar() {
     return (
-
-        <Nav className="me-auto">
-            <Nav.Link href="#features">Add Student</Nav.Link>
-            <Nav.Link href="#pricing">View Students</Nav.Link>
-        </Nav>
-    
+<div>
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Link to="/" className='navbar-brand'>Student Management System</Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Link to="/student" className="nav-link">Add Student</Link>
+            <Link to="/listStudents" className="nav-link">View Students</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
   )
-    }
+}
+    
+ 
